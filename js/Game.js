2bd8d899527897c1f79038206eadba4f6a115257
nishtha2005 <nishthashrivas62@gmail.com>
2021-07-19 12:36:17 +0530

    highlight

--- js/Game.js
+++ js/Game.js
@@ -70,6 +70,9 @@ class Game {
         cars[index-1].y = y;
 
         if (index === player.index){
+          stroke(3);
+          fill("yellow")
+          ellipse(x,y,100,100)
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y;
